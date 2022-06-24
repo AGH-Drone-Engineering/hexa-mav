75f0f7c7ccb610f8da5dfcd9321deab7a211eea2
atomwoz <84351511+atomwoz@users.noreply.github.com>
2022-06-24 12:59:43 +0200

    Fixed bugs...

--- main.py
+++ main.py
@@ -17,14 +17,15 @@ SHOOT_MSG = "SHOOT"
 PIN_STRZAL = 11
 PIN_1 = 9
 PIN_2 = 10
+#DEVICE = 'udpout:192.168.1.101:14560'
 DEVICE = '/dev/serial/by-id/usb-Hex_ProfiCNC_CubeOrange_270022000D51393239383638-if00'
 BAUD = 115200
 # BAUD = 57600
 # BAUD = 9600
 TARGET_ALTITUDE = 2
 
-thrd_num = 0
 conn = None
+watcher_running = 0
 
 
 def check_ok():
@@ -43,11 +44,18 @@ def map_val(x, in_min, in_max, out_min, out_max):
 def map_servo(x):
     return int(map_val(x, 0, 180, 500, 2500))
 
+def wait_conn():
+    msg = None
+    while not msg:
+       the_connection.mav.ping_send(
+            int(time.time() * 1e6), 0,  0,  0 )
+       msg = the_connection.recv_match()
+       time.sleep(0.5)
 
 def getpos():
     print("test")
     the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component,
-                                         mavutil.mavlink.MAV_CMD_REQUEST_MESSAGE, 33, 0, 0, 0, 0, 0, 0)
+                                         mavutil.mavlink.MAV_CMD_REQUEST_MESSAGE,0, 33, 0, 0, 0, 0, 0, 0)
     xx = the_connection.recv_match(type='GLOBAL_POSITION_INT', blocking=True).to_dict()
     print("test")
     print(xx)
@@ -69,15 +77,14 @@ def setpos():
     the_connection.mav.send(
         mavutil.mavlink.MAVLink_set_position_target_global_int_message(10, the_connection.target_system,
                                                                        the_connection.target_component,
-                                                                       mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
-                                                                       int(0b110111111000), int(float(xx[0]) * 10 ** 7),
+                                                                       mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,0, int(0b110111111000), int(float(xx[0]) * 10 ** 7),
                                                                        int(float(xx[1]) * 10 ** 7), float(getalt()),
                                                                        0, 0, 0))
 
 
 def moveServo(odl, pin):
     the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component,
-                                         mavutil.mavlink.MAV_CMD_DO_SET_SERVO, 0, pin, odl, 0, 0, 0, 0)
+                                         mavutil.mavlink.MAV_CMD_DO_SET_SERVO, 0, 0, pin, odl, 0, 0, 0, 0)
 
 
 def strzelaj(kulka):
@@ -96,11 +103,10 @@ def strzelaj(kulka):
 
 
 def arm(force=21196):
-    the_connection.set_mode_auto()
     the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component,
-                                         mavutil.mavlink.MAV_CMD_NAV_GUIDED_ENABLE, 1, 0, 0, 0, 0, 0, 0)
+                                         mavutil.mavlink.MAV_CMD_NAV_GUIDED_ENABLE, 0 ,1, 0, 0, 0, 0, 0, 0)
     the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component,
-                                         mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 0, 1, force, 0, 0, 0, 0)
+                                         mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,0, 0, 1, force, 0, 0, 0, 0)
     s = check_ok()
     if s:
         print("Uzbrojono pojazd")
@@ -110,30 +116,30 @@ def arm(force=21196):
 
 def disarm():
     the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component,
-                                         mavutil.mavlink.MAV_CMD_NAV_GUIDED_ENABLE, 0, 0, 0, 0, 0, 0, 0)
+                                         mavutil.mavlink.MAV_CMD_NAV_GUIDED_ENABLE, 0, 0, 0, 0, 0, 0, 0, 0)
     the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component,
-                                         mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 0, 1, 21196, 0, 0, 0, 0)
+                                         mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,0, 0, 1, 21196, 0, 0, 0, 0)
     the_connection.set_mode_manual()
 
 
 def rth():
     the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component,
-                                         mavutil.mavlink.MAV_CMD_NAV_RETURN_TO_LAUNCH, 0, 0, 0, 0, 0, 0, 0)
+                                         mavutil.mavlink.MAV_CMD_NAV_RETURN_TO_LAUNCH,0, 0, 0, 0, 0, 0, 0, 0)
 
 
 def takeoff():
     the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component,
-                                         mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 0, 0, 0, 0, TARGET_ALTITUDE)
+                                         mavutil.mavlink.MAV_CMD_NAV_TAKEOFF,0, 0, 0, 0, 0, 0, 0, TARGET_ALTITUDE)
 
 
 def land():
     the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component,
-                                         mavutil.mavlink.MAV_CMD_NAV_LAND, 0, 0, 0, 0, 0, 0, 0)
+                                         mavutil.mavlink.MAV_CMD_NAV_LAND,0, 0, 0, 0, 0, 0, 0, 0)
 
 def getfullpos():
-    print("Nadawanie zadania pozycji")
+    print("Nadawanie prosby o pozycje")
     the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component,
-                                         mavutil.mavlink.MAV_CMD_REQUEST_MESSAGE, 33, 0, 0, 0, 0, 0, 0)
+                                         mavutil.mavlink.MAV_CMD_REQUEST_MESSAGE,0, 33, 0, 0, 0, 0, 0, 0)
     xx = the_connection.recv_match(type='GLOBAL_POSITION_INT', blocking=True).to_dict()
     print("Odebrano pozycje drona")
     print(xx)
@@ -161,29 +167,33 @@ def watcher():
         else:
             time.sleep(1)
 
-    thrd_num -= 1
-    if thrd_num == 0:
         conn.sendall(b"ACK\n")
+        watcher_running = 0
 
 
 def start_ack_watcher(x):
     global thrd_num
-    thrd = threading.Thread(target=watcher, name="ack pos watcher")
-    thrd.start()
-    thrd_num += 1
+    if not watcher_running:
+        thrd = threading.Thread(target=watcher, name="ack pos watcher")
+        thrd.start()
+        watcher_running = 1
 
 
 # the_connection = mavutil.mavlink_connection(DEVICE)
 the_connection = mavutil.mavlink_connection(DEVICE, BAUD)
 
-moveServo(1100, PIN_STRZAL)
-print("Ustawianie silnika na IDLE")
+print("Start!, czekam na ping od drona")
+wait_conn()
+print("Odebrano PING")
 the_connection.wait_heartbeat()
 print("Odebrano heartbeat")
+moveServo(500, PIN_2)
+moveServo(500, PIN_1)
 moveServo(1100, PIN_STRZAL)
+print("Ustawiono silniki/serwa w tryb IDLE")
 print("Czekam na GPS")
 the_connection.wait_gps_fix()
-print("GPS fixed... lecimy")
+print("GPS ustawiony")
 # print("Serwo IDLE")
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sck:
@@ -229,9 +239,14 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sck:
                     else:
                         print("[OSTRZEZENIE] Odebrano niepoprawny format komendy", SHOOT_MSG)
 
+                if data.startswith("SERVO-RAW"):
+                    moveServo(int(data[len("SERVO-RAW") + 1:]), PIN_1)
+                    print("Poruszam servem", int(data[len("SERVO_RAW") + 1:]), PIN_1)
+
                 if data.startswith("SERVO"):
-                    moveServo(int(data[len("SERVO") + 1:]), PIN_1)
-                    print("Poruszam servem", map_servo(int(data[len("SERVO") + 1:])), PIN_1)
+                    a = int(map_servo(int(data[len("SERVO-RAW") + 1:])))
+                    moveServo(a, PIN_1)
+                    print("Poruszam servem", a, PIN_1)
 
                 if data == "FORCE-ARM":
                     arm()
